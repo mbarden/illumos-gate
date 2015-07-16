a248eb46eec7428fc215a5a0044bdd4d1cdd550f
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2015-07-16 19:17:32 -0400

    Document the ARMv7 cache types

--- usr/src/uts/armv7/vm/vm_machdep.c
+++ usr/src/uts/armv7/vm/vm_machdep.c
@@ -25,12 +25,7 @@
  */
 
 /*
- * Let's talk about page coloring on ARMv6. ARMv6 uses VIPT caches. Assuming a
- * 4k page, the lower two bits are subject to page coloring. Therefore we need 4
- * page colors. Thinking ahead, some parts of ARMv7 have PIPT caches, but still
- * have VIPT instruction caches. Therefore, we'll initiailize the hw_page_array
- * with the default amount of page coloring -- 4, but allow an individual
- * platform to overwrite that as we initialize the MMU.
+ * ARMv7 has PIPT data/unified caches and VIVT instruction caches.
  *
  * In addition, we always set vac_colors to one to indicate that we need virtual
  * address caching.
