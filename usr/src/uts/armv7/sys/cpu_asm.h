119dbb49fae311eaab9dae2a5d2f6485b05cd228
Matt Barden <otherst00f@gmail.com>
2015-10-28 18:46:07 -0400

    armv7: fix cpu mode defines, add SCTLR_Z bit

--- usr/src/uts/armv7/sys/cpu_asm.h
+++ usr/src/uts/armv7/sys/cpu_asm.h
@@ -21,12 +21,15 @@ extern "C" {
  */
 #define	CPU_MODE_USR	0x10	/* User mode */
 #define	CPU_MODE_FIQ	0x11	/* Fast interrupt */
+#define	CPU_MODE_IRQ	0x12	/* Interrupt */
 #define	CPU_MODE_SVC	0x13	/* Supervisor */
 #define	CPU_MODE_MON	0x16	/* Monitor (security extension) */
 #define	CPU_MODE_ABT	0x17	/* Abort (memory/insn access or breakpoint) */
-#define	CPU_MODE_IRQ	0x18	/* Interrupt */
+#define	CPU_MODE_HYP	0x1a	/* Hypervisor mode */
 #define	CPU_MODE_UND	0x1b	/* Undefined instruction */
 #define	CPU_MODE_SYS	0x1f	/* System */
+
+#define CPU_MODE_MASK	0x1f
 
 /*
  * ARM Exceptions (by their index in the exception vector)
@@ -42,6 +45,7 @@ extern "C" {
 #define	ARM_EXCPT_NUM		8
 
 #define	ARM_SCTLR_C		0x0004
+#define	ARM_SCTLR_Z		0x0800
 #define	ARM_SCTLR_I		0x1000
 
 #ifdef	_ASM
